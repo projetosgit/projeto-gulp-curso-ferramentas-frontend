f5756a50dcfb18ed33b823bc7082465d71b5d1cd
Daniel Tapias Morales <tapmorales@gmail.com>
2019-12-30 14:16:59 +0000

    finish js task

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,41 @@ const browsersync = require('browser-sync').create();
 
 
 function sass(){
+
+    const path = isProd ? './dist/css' : './src/css'
+
     return gulp.src('./src/scss/**/*.scss')
         .pipe(gulpsass())
-        .pipe(gulp.dest('./src/css'))
+        .pipe(gulp.dest(path))
 }
 
 function js(){
-    return gulp.src([
-            './src/js/getStudents.js',
-            './src/js/getMedia.js',
-            './src/js/getRandomNumber.js',
-            './src/js/app.js'
-        ])
+
+    const files = [
+        './node_modules/babel-polyfill/dist/polyfill.js',
+        './src/js/getStudents.js',
+        './src/js/getMedia.js',
+        './src/js/getRandomNumber.js',
+        './src/js/app.js'
+    ]
+
+    if(isProd) {
+        return gulp.src(files)
+            .pipe(concat('app.min.js'))
+            .pipe(babel({
+                "presets": [
+                    ["env", {
+                        "targets": {
+                            "browsers": [">1%", "last 2 versions"]
+                        }
+                    }]
+                ]
+            }))
+            .pipe(uglify())
+            .pipe(gulp.dest('./dist/js'))
+    } 
+
+    return gulp.src(files)
         .pipe(sourcemaps.init())
         .pipe(concat('app.min.js'))
         .pipe(sourcemaps.write('.'))
@@ -34,7 +57,31 @@ function server(){
 
     gulp.watch(['./src/js/**/*.js', '!./src/js/**/*.min.js'], js)
         .on('change', browsersync.reload)
+
+    gulp.watch('./src/**/*.html').on('change', browsersync.reload)
+
+    gulp.watch('./src/scss/**/*.scss', sass).on('change', browsersync.reload)
 }
 
-exports.default = js
-exports.server = server
+let isProd = false;
+function prod(cb){
+    isProd = true
+    cb()
+}
+function dev(cb){
+    isProd = false
+    cb()
+}
+
+function html(){
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest('./dist'))
+}
+
+function clear(){
+    return del('./dist')
+}
+
+exports.default = gulp.series(dev, clear, gulp.parallel(html, js, sass), server)
+
+exports.build = gulp.series(prod, clear, gulp.parallel(html, js, sass))

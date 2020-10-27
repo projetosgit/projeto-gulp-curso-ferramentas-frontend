ccd8f441b854c762221bd9ca524dd2af4cb4263e
Artur da Silva Rezende <projetosgit4@gmail.com>
2020-10-27 15:53:08 -0300

    feature sass works

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
-const gulp = require('gulp');
-const gulpsass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const del = require('del');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const babel = require('gulp-babel');
-const browsersync = require('browser-sync').create();
+const gulp = require("gulp");
+const gulpsass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const del = require("del");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+const browsersync = require("browser-sync").create();
 
+function sass() {
+    return gulp.src('./src/scss/**/*.scss')
+    .pipe(gulpsass())
+    .pipe(gulp.dest('./src/css'))
+}
+
+module.exports.default = sass;
